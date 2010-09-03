f495042f1798310c53c1417f87e0b53142e0160f
Adam McCrea <adam@edgecase.com>
2010-09-03 14:16:11 -0400

    move chronic parsing to model

--- lib/log_entry.rb
+++ lib/log_entry.rb
@@ -8,12 +8,13 @@ class LogEntry
       db.collection('production_log')
     else
       conn = Mongo::Connection.from_uri('mongodb://localhost')
-      db   = conn.db('janova_staging')
-      db.collection('staging_log')
+      db   = conn.db('logginator_dev')
+      db.collection('log_entries')
     end
   end
   
   def self.page(page, params = {})
+    params ||= {}
     options = page_opts(page)
     options[:fields] = %w{ request_time controller action }
 
@@ -21,6 +22,7 @@ class LogEntry
   end
 
   def self.find_one(id_or_doc, params = {})
+    params ||= {}
     object_id = case id_or_doc
       when BSON::OrderedHash then id_or_doc['_id']
       when String then BSON.ObjectId(id_or_doc)
@@ -40,14 +42,14 @@ class LogEntry
     def self.clean_params(params)
       params = params.reject { |k,v| v.nil? || (v.respond_to?(:empty?) && v.empty?) }
 
-      if params['from']
+      if from = Chronic.parse(params.delete('from'))
         params['request_time'] ||= {}
-        params['request_time'][:$gt] = params.delete('from')
+        params['request_time'][:$gt] = from.utc
       end
 
-      if params['to']
+      if to = Chronic.parse(params.delete('to'))
         params['request_time'] ||= {}
-        params['request_time'][:$lt] = params.delete('to')
+        params['request_time'][:$lt] = to.utc
       end
 
       params.delete('param')
--- logginator.rb
+++ logginator.rb
@@ -8,9 +8,6 @@ require 'lib/helpers'
 
 get '/' do
   @page = 1
-  params['q'] ||= {}
-  @from = params['q']['from'] = Chronic.parse(params['q']['from']) || Chronic.parse('1/1/10')
-  @to   = params['q']['to']   = Chronic.parse(params['q']['to']) || Time.now
   @log_entries = LogEntry.page(@page, params['q'])
   @log_entry   = LogEntry.find_one(@log_entries.first)
   haml :results
@@ -18,9 +15,6 @@ end
 
 get '/page/:page' do |page|
   @page = page.to_i
-  params['q'] ||= {}
-  @from = params['q']['from'] = Chronic.parse(params['q']['from']) || Chronic.parse('1/1/10')
-  @to   = params['q']['to']   = Chronic.parse(params['q']['to']) || Time.now
   @log_entries = LogEntry.page(@page, params['q'])
   @log_entry   = LogEntry.find_one(@log_entries.first)
   haml :results
@@ -28,9 +22,6 @@ end
 
 get '/page/:page/entry/:id' do |page, id|
   @page = page.to_i
-  params['q'] ||= {}
-  @from = params['q']['from'] = Chronic.parse(params['q']['from']) || Chronic.parse('1/1/10')
-  @to   = params['q']['to']   = Chronic.parse(params['q']['to']) || Time.now
   @log_entries = LogEntry.page(@page, params['q'])
   @log_entry   = LogEntry.find_one(id)
   haml :results
